59f88ae5c9ee552d5041ee0404afb13833e65a32
Jose Noriega <josenoriega723@gmail.com>
2022-05-22 20:14:51 -0700

    feat: add copy command

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = new Vue({
   el: '#app',
   data() {
     return {
-      message: 'hello',
+      copied: false,
       owner: {
         r: 1,
         w: 0,
@@ -54,11 +54,11 @@ const app = new Vue({
       if (object.x) {
         output += 1
       }
-      
+
       if (object.w) {
         output += 2
       }
-      
+
       if (object.r) {
         output += 4
       }
@@ -71,13 +71,13 @@ const app = new Vue({
       } else {
         output += '-'
       }
-      
+
       if (object.w) {
         output += 'w'
       } else {
         output += '-'
       }
-      
+
       if (object.x) {
         output += 'x'
       } else {
@@ -85,6 +85,20 @@ const app = new Vue({
       }
 
       return output;
-    }
+    },
+    copyToClipboard() {
+      this.copied = true;
+      const command = `sudo chmod ${this.fullOctal}`;
+      // add to clipboard
+      const el = document.createElement('textarea');
+      el.value = command;
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+    },
   },
 });
